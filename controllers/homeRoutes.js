238c81f53b63ec3a02b3e967b419fa6bd5768455
GitPit <rifflifter@gmail.com>
2021-07-26 20:35:35 -0400

    updated post and delete routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,9 +26,65 @@ router.get('/', async (req,res) => {
 }
 });
 
+//
+
+router.get("/map", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await user.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+    const loggedUser = userData.get({ plain: true });
+    res.render("map", {
+      ...user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 
 //post route
+
+router.post('/', async (req, res) => {
+  try { 
+    const newPost = await post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      username: req.session.username
+    });
+    // const imageUrl = await Post.create({res.result.info.secure_url});
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete route
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 // Prevent non logged in users from viewing the homepage
