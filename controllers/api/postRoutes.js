24febafee443d3f0eec577983afdbd8421f580d4
GitPit <rifflifter@gmail.com>
2021-07-29 17:19:13 -0400

    updated postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { user } = require('../../models')
 const { post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
@@ -14,6 +15,26 @@ router.post('/', withAuth, async (req, res) => {
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  console.log(req.body)
+  try {
+    const updatePost = await post.update({
+      // image: req.body.image,
+      likes: req.body.likes,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+    let responseBack = await post.findByPk(req.params.id);
+
+    res.status(200).json(responseBack);
+  } catch (err) {
+    res.status(400).json(err)
   }
 });
 
