4e860b97b5187156b35af4b92893982835901cc1
GitPit <rifflifter@gmail.com>
2021-07-29 22:53:54 -0400

    fixed syntax error on postHandler.js

--- public/postHandler.js
+++ public/postHandler.js
@@ -5,20 +5,19 @@ const newPostHandler = async (event) =>
   const content = document.querySelector("#post-content").value.trim();
 
   if (title && content) {
+    console.log(title, content)
       fetch("/api/post", {
       method: "POST",
       body: JSON.stringify({ title, content }),
       headers: { "Content-Type": "application/json" },
-    }).then((response)=> {
+    }).then((response) => {
     if (response.ok) {
       document.location.replace("/homepage");
     } else {
       alert("Please fill out title and content fields before submitting.")
     }
   }
-
- 
-};
+    )};
 
 document.getElementById("postBtn").addEventListener(
 "click",
