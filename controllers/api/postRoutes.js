24d239949970328ee47a38c445410a4978d30159
GitPit <rifflifter@gmail.com>
2021-08-05 14:03:54 -0400

    changed status for error/catch on postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -30,19 +30,35 @@ const withAuth = require('../../utils/auth');
 /////
 
 
-router.post('/', withAuth, async (req, res) => {
-  try {
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const newPost = await post.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//       // username: req.session.username
+//     });
+
+//     console.log(req.title);
+
+//     res.status(200).json(newPost);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// new:
+
+router.post('/', async (req, res) => {
+  try { 
     const newPost = await post.create({
       ...req.body,
       user_id: req.session.user_id,
-      // username: req.session.username
+      username: req.session.username
     });
-
-    console.log(req.title);
-
+    
     res.status(200).json(newPost);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
