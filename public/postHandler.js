7975ef1a2d28958a9609ce35770c6439f15d11f6
GitPit <rifflifter@gmail.com>
2021-08-05 20:26:54 -0400

    corrected scope on postHandler.js

--- public/postHandler.js
+++ public/postHandler.js
@@ -1,33 +1,35 @@
 const newPostHandler = async (event) =>
 
-{
-  const title = document.querySelector("#post-title").value.trim();
-  const content = document.querySelector("#post-content").value.trim();
+  {
+    event.preventDefault();
+    const title = document.querySelector("#post-title").value.trim();
+    const content = document.querySelector("#post-content").value.trim();
 
-  if (title && content) {
-    console.log(title, content);
-      fetch("/api/post", {
-      method: "POST",
-      body: JSON.stringify({ title, content }),
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-    if (response.ok) {
-      document.location.replace("/homePage");
-    } else {
-      alert("Please fill out title and content fields before submitting.")
-    }
-  }
-    )};
+    if (title && content) {
+      console.log(title, content);
+      fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          content
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        },
+      }).then((response) => {
+        if (response.ok) {
+          document.location.replace("/");
+        } else {
+          alert("Please fill out title and content fields before submitting.")
+        }
+      })
+    };
 
-document.getElementById("postBtn").addEventListener(
-"click",
+  };
 
-function (e) {
-e.preventDefault();
-console.log("clicked!!!!");
-},
-false
-)};
+  document.getElementById("postBtn").addEventListener(
+    "click",
 
+    newPostHandler
+  )
 
-newPostHandler();
